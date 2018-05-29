7c2f6dd25ad320dca994fb3d13d3158d795b401b
Gary Fredericks <fredericksgary@gmail.com>
2018-05-29 06:27:47 -0500

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  com.gfredericks/org-editor "0.1.4"
+(defproject  com.gfredericks/org-editor "0.1.5-SNAPSHOT"
   :description "Some clojure code for reading and writing org files"
   :url "https://github.com/gfredericks/org-editor"
   :license {:name "Eclipse Public License"
